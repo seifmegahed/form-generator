1389548cbc309828c69b2700069a409e5ecea3dc
seifmegahed <seifmegahed@me.com>
2024-08-15 15:49:34 +0300

    basic form tester

--- apps/example/src/app/page.tsx
+++ apps/example/src/app/page.tsx
@@ -1,5 +1,102 @@
+"use client";
+
+import { z } from "zod";
+import { useState } from "react";
+import FormTester from "@/components/form-builder-tester";
+import { emptyToUndefined } from "@/components/form-builder";
+import { FieldType } from "@/components/form-builder";
+import { Button } from "@/components/ui/button";
+
+const formFields = [
+  {
+    name: "firstName",
+    label: "First Name",
+    className: "md:col-span-2",
+    type: FieldType.Text,
+    default: "",
+    schema: z.preprocess(emptyToUndefined, z.string()),
+  },
+  {
+    name: "lastName",
+    label: "Last Name",
+    className: "md:col-span-2",
+    type: FieldType.Text,
+    default: "",
+    schema: z.preprocess(emptyToUndefined, z.string()),
+  },
+  {
+    name: "major",
+    label: "Major",
+    className: "md:col-span-3",
+    type: FieldType.Select,
+    default: undefined,
+    options: ["Computer Science", "Mathematics", "Physics"],
+    schema: z.string(),
+  },
+  {
+    name: "age",
+    label: "Age",
+    type: FieldType.Number,
+    default: 0,
+    schema: z.preprocess(
+      emptyToUndefined,
+      z.preprocess(
+        (val) => Number(val),
+        z.number({ invalid_type_error: "Age must be a number" })
+      )
+    ),
+  },
+  {
+    name: "isStudent",
+    label: "Is Student",
+    className: "md:col-span-4",
+    type: FieldType.Checkbox,
+    default: false,
+    schema: z.boolean(),
+  },
+  {
+    name: "notes",
+    label: "Notes",
+    className: "md:col-span-4",
+    type: FieldType.Textarea,
+    default: "",
+    schema: z.string(),
+  },
+];
+
 export default function HomePage() {
+  const [fields, setFields] = useState(formFields);
+
+  const newField = (label: string) => ({
+    name: label.toLowerCase().replace(" ", "-"),
+    label,
+    type: FieldType.Text,
+    default: "",
+    className: "md:col-span-2",
+    schema: z.preprocess(emptyToUndefined, z.string()),
+  });
+  
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+
   return (
-    <div></div>
+    <>
+      <div className="flex justify-center items-center min-h-screen flex-col gap-5 md:px-5 py-5">
+        <FormTester formFields={fields} />
+        <Button
+          className="w-60"
+          onClick={() => {
+            setFields((prev) => [...prev, newField(`Field ${fields.length}`)]);
+            scrollToTop();
+          }}
+        >
+          Add Field
+        </Button>
+      </div>
+    </>
   );
 }
---
+++ apps/example/src/components/form-builder-tester/index.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { FormGenerator, FieldDataType } from "@/components/form-builder";
+
+import { Button } from "../ui/button";
+import { useMemo } from "react";
+
+function FormTester({ formFields }: { formFields: FieldDataType[] }) {
+  const formData = useMemo(
+    () => new FormGenerator<typeof formFields>(formFields),
+    [formFields]
+  );
+
+  type schemaDataType = z.infer<typeof formData.schema>;
+
+  const form = useForm<schemaDataType>({
+    resolver: zodResolver(formData.schema),
+    defaultValues: formData.defaultValues,
+  });
+
+  const onSubmit = (data: schemaDataType) => {
+    console.log(data);
+    form.reset();
+  };
+
+  return (
+    <form
+      className="w-full max-w-lg bg-primary-foreground p-5 sm:rounded-lg shadow-md"
+      onSubmit={form.handleSubmit(onSubmit)}
+    >
+      <div className="text-3xl font-bold py-3">
+        <p className="text-primary  line-clamp-1 text-ellipsis">
+          Form Builder Tester
+        </p>
+      </div>
+      <div className="grid md:grid-cols-4 gap-x-3">
+        {formData.fields<typeof formFields, schemaDataType>({
+          form,
+        })}
+      </div>
+      <div className="flex justify-end py-5">
+        <Button
+          type="submit"
+          className="rounded-md px-12 py-2 font-medium"
+          disabled={form.formState.isSubmitting}
+        >
+          Save
+        </Button>
+      </div>
+    </form>
+  );
+}
+
+export default FormTester;
