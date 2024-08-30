c9dff2a563922bda8efd76235a7ca0df4942069d
seifmegahed <seifmegahed@me.com>
2024-08-30 20:40:04 +0300

    All field options selected by default

--- cli/src/config/index.ts
+++ cli/src/config/index.ts
@@ -30,17 +30,17 @@ export const configPrompts: PromptObject[] = [
     type: "multiselect",
     name: "fields",
     message:
-      "Select the fields you want to include in the component \n (Use space to select multiple)",
+      "Select the fields you want to include in the component \n (Use space to select/unselect multiple)",
 
     instructions: false,
     min: 1,
 
     choices: [
       { title: "Text", value: FieldOptions.Text, selected: true },
-      { title: "Select", value: FieldOptions.Select },
-      { title: "Textarea", value: FieldOptions.Textarea },
-      { title: "Checkbox", value: FieldOptions.Checkbox },
-      { title: "Date Picker", value: FieldOptions.DatePicker },
+      { title: "Select", value: FieldOptions.Select, selected: true },
+      { title: "Textarea", value: FieldOptions.Textarea, selected: true },
+      { title: "Checkbox", value: FieldOptions.Checkbox, selected: true },
+      { title: "Date Picker", value: FieldOptions.DatePicker, selected: true },
     ],
   },
 ];
