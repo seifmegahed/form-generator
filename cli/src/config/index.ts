b36a5b84a248a3210b2db7a4c7708d0d19555ab2
seifmegahed <seifmegahed@me.com>
2024-11-16 12:37:07 +0200

    cli fixes

--- cli/src/config/index.ts
+++ cli/src/config/index.ts
@@ -9,13 +9,13 @@ export const configPrompts: PromptObject[] = [
   {
     type: "text",
     name: "components",
-    message: `Configure the import alias for ${highlight("components")}:`,
+    message: `Enter import alias for ${highlight("components")}:`,
     initial: DEFAULT_COMPONENTS,
   },
   {
     type: "text",
     name: "utils",
-    message: `Configure the import alias for ${highlight("utils")}:`,
+    message: `Enter import alias for ${highlight("utils")}:`,
     initial: DEFAULT_UTILS,
   },
   {
@@ -33,7 +33,6 @@ export const configPrompts: PromptObject[] = [
       "Select the fields you want to include in the component \n (Use space to select/unselect multiple)",
     instructions: false,
     min: 1,
-
     choices: [
       { title: "Text", value: FieldOptions.Text, selected: true },
       { title: "Select", value: FieldOptions.Select, selected: true },
